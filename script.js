03896cb18a48d9767b82dfed19e00c3c1f0a9cb9
Prasanna <prasannaacharya2073@gmail.com>
2023-02-26 06:55:45 +0545

    Traffic rule corrected for green and yellow lights

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Change the light-color in traffic-light
 const redLight = document.getElementById("redLight");
-const greenLight = document.getElementById("greenLight");
 const yellowLight = document.getElementById("yellowLight");
+const greenLight = document.getElementById("greenLight");
 const car = document.getElementById("car");
 const wheel1 = document.getElementById("wheel1");
 const wheel2 = document.getElementById("wheel2");
@@ -13,36 +12,42 @@ function carMovement() {
 
 setInterval(carMovement, 8000);
 
+// Change the light-color in traffic-light
 function changeLightColor() {
   if (redLight.classList.contains("active")) {
     redLight.classList.remove("active");
-    yellowLight.classList.remove("active");
-    greenLight.classList.add("active");
-  } else if (yellowLight.classList.contains("active")) {
-    yellowLight.classList.remove("active");
-    redLight.classList.add("active");
+    yellowLight.classList.add("active");
     greenLight.classList.remove("active");
   } else if (greenLight.classList.contains("active")) {
     greenLight.classList.remove("active");
+    redLight.classList.add("active");
+    yellowLight.classList.remove("active");
+  } else if (yellowLight.classList.contains("active")) {
+    yellowLight.classList.remove("active");
     redLight.classList.remove("active");
-    yellowLight.classList.add("active");
+    greenLight.classList.add("active");
   }
 }
 
+// Check the movement of the car
 function checkCarMovement() {
   if (redLight.classList.contains("active")) {
     // console.log("This is triggered!");
     car.style.animation = "none";
     wheel1.style.animation = "none";
     wheel2.style.animation = "none";
-  } else if (yellowLight.classList.contains("active")) {
+  } else if (greenLight.classList.contains("active")) {
     // console.log("No, this is triggered!");
     car.style.animation = "center-to-left 3s linear 1";
     wheel1.style.animation = "wheels-rotation 1.5s linear infinite";
     wheel2.style.animation = "wheels-rotation 1.5s linear infinite";
-
-    // car.style.animation = "right-to-center 7s linear 1";
+    // if (yellowLight.classList.contains("active")) {
+    //   car.style.animation = "right-to-center 7s linear 1";
+    //   wheel1.style.animation = "wheels-rotation 1.5s linear infinite";
+    //   wheel2.style.animation = "wheels-rotation 1.5s linear infinite";
+    // }
   }
+  // car.style.animation = "right-to-center 7s linear 1";
 }
 
 // function checkCarMovement2 {
